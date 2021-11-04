af1c0d5bc2fd68aae0284c969317df0aa9147aac
SoHyeon Kim <happythgus89@gmail.com>
2021-11-04 12:21:23 +0900

    DivideAndConquer

--- DivideAndConquer/karatsuba.cpp
+++ DivideAndConquer/karatsuba.cpp
@@ -1,7 +1,84 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void normalize(vector <int>& num) {
+	num.push_back(0);
+
+	for (int i = 0; i + 1 < num.size(); ++i) {
+		if (num[i] < 0) {
+			int borrow = (abs(num[i]) + 9) / 10;
+			num[i + 1] -= borrow;
+			num[i] += borrow * 10;
+		}
+		else {
+			num[i + 1] += num[i] / 10;
+			num[i] %= 10;
+		}
+	}
+	while (num.size() > 1 && num.back() == 0) {
+		num.pop_back();
+	}
+}
+
+vector <int> multiply(const vector<int>& a, const vector<int>& b) {
+	vector<int> c(a.size() + b.size() + 1, 0); // 0으로 초기화
+	for (int i = 0; i < a.size(); ++i) {
+		for (int j = 0; j < b.size(); ++j) {
+			c[i + j] += a[i] * b[j];
+		}
+	}
+	normalize(c); 
+	return c;
+}
 
 
 
-int main() {
+
+// 카라츠바 알고리즘 구현
+
+
+void addTo(vector<int>& a, const vector<int>& b, int k);
+void subForm(vector<int>& a, const vector<int>& b);
+
+vector <int> karatsuba(const vector<int>& a, const vector<int>& b) {
+	int an = a.size();
+	int bn = b.size();
+
+	if (an < bn) {
+		return karatsuba(b, a);
+	}
+
+	if (an == 0 || bn == 0) {
+		return vector<int>();
+	}
+
+	if (an < 50) {
+		return multiply(a, b);
+	}
+
+	int half = an / 2;
+	vector <int> a0(a.begin(), a.begin() + half);
+	vector <int> a1(a.begin() + half, a.end());
+
+	vector <int> b0(b.begin(), b.begin() + min<int>(b.size(), half));
+	vector <int> b1(b.begin() + min<int>(b.size(), half), b.end());
+
+	// z2 = a * b1
+	vector <int> z2 = karatsuba(a1, b1);
+	vector <int> z0 = karatsuba(a0, b0);
+	addTo(a0, a1, 0);
+	addTo(b0, b1, 0);
+
+	vector <int> z1 = karatsuba(a0, b0);
+	subForm(z1, z0);
+	subForm(z1, z2);
+
+	vector <int> ret;
+	addTo(ret, z0, 0);
+	addTo(ret, z1, half);
+	addTo(ret, z2, half + half);
+	return ret;
 
 }
+
