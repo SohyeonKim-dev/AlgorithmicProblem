bb07262a4840e054a939b960002cfdd4a1317e27
SoHyeon Kim <happythgus89@gmail.com>
2021-11-30 13:53:04 +0900

    GreedyMethod

--- GreedyMethod/matchOrder.cpp
+++ GreedyMethod/matchOrder.cpp
@@ -9,5 +9,14 @@ int order(const vector<int>& russian, const vector<int>& korean) {
 	int wins = 0;
 
 	multiset <int> ratings(korean.begin(), korean.end());
-
+	for (int rus = 0; rus < n; ++rus) {
+		if (*ratings.rbegin() < russian[rus]) {
+			ratings.erase(ratings.begin());
+		}
+		else {
+			ratings.erase(ratings.lower_bound(russian[rus]));
+			++wins;
+		}
+	}
+	return wins;
 }
